ef4349eac7d9540a5811c84af24827e30bb0d99f
paigeman <53284808+paigeman@users.noreply.github.com>
2025-05-13 17:48:27 +0800

    Finish client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/paigeman/r1-BDVA/cmd/config"
 	"github.com/paigeman/r1-BDVA/cmd/model"
+	"log"
 	"math/rand"
 	"net"
 	"sync"
@@ -13,45 +14,57 @@ func simulateBus(id string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	addr, err := net.ResolveUDPAddr("udp", config.ServerIP+":"+config.ServerPort)
 	if err != nil {
-		fmt.Println("Error resolving address:", err)
+		log.Println(id, ": Error resolving address:", err)
 		return
 	}
 	conn, err := net.DialUDP("udp", nil, addr)
 	if err != nil {
-		fmt.Println("Error connecting:", err)
+		log.Println(id, ": Error connecting:", err)
 		return
 	}
 	defer func(conn *net.UDPConn) {
 		err := conn.Close()
 		if err != nil {
-			fmt.Println("Error closing connection:", err)
+			log.Println(id, ": Error closing connection:", err)
 		}
 	}(conn)
 	// 确保每辆车生成的随机数不一致
-	rand.New(rand.NewSource(model.GenerateBusRandomSeed(id)))
+	r := rand.New(rand.NewSource(model.GenerateBusRandomSeed(id)))
+	// 初始经纬度可以调整
+	latitude := 23.12
+	longitude := 113.28
+	busStatus := model.BusStatus{
+		BusID:     id,
+		Latitude:  latitude,
+		Longitude: longitude,
+		Speed:     0,
+	}
+	busStatus.Now()
+	for {
+		data, err := json.Marshal(&busStatus)
+		if err != nil {
+			log.Println(id, ": Error marshalling busStatus:", err)
+		}
+		_, err = conn.Write(data)
+		if err != nil {
+			log.Println(id, ": Error writing busStatus:", err)
+		}
+		//time.Sleep()
+		// 更新状态
+		err = busStatus.Move(r)
+		if err != nil {
+			log.Println(id, ": Error moving busStatus:", err)
+		}
+	}
 }
 
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", config.ServerIP+":"+config.ServerPort)
-	if err != nil {
-		fmt.Println("Error resolving address:", err)
-		return
+	numOfBuses := 10
+	idSet := model.GenerateBusIDs(numOfBuses)
+	var wg sync.WaitGroup
+	for _, id := range idSet {
+		wg.Add(1)
+		go simulateBus(id, &wg)
 	}
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		fmt.Println("Error connecting:", err)
-		return
-	}
-	defer func(conn *net.UDPConn) {
-		err := conn.Close()
-		if err != nil {
-			fmt.Println("Error closing connection:", err)
-		}
-	}(conn)
-	message := []byte("Hello, UDP Server!")
-	if _, err = conn.Write(message); err != nil {
-		fmt.Println("Error sending:", err)
-		return
-	}
-	fmt.Println("Message sent successfully")
+	wg.Wait()
 }
--- cmd/model/bus.go
+++ cmd/model/bus.go
@@ -3,6 +3,8 @@ package model
 import (
 	"fmt"
 	"hash/fnv"
+	"math"
+	"math/rand"
 	"time"
 )
 
@@ -36,4 +38,44 @@ func GenerateBusRandomSeed(id string) int64 {
 	}
 	// 做哈希扰动
 	return time.Now().UnixNano() ^ int64(h.Sum64())
+}
+
+func haversine(lat1, lng1, lat2, lng2 float64) float64 {
+	// 地球半径（米）
+	const R = 6371000
+	dLat := (lat2 - lat1) * math.Pi / 180
+	dLng := (lng2 - lng1) * math.Pi / 180
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(lat1*math.Pi/180)*math.Cos(lat2*math.Pi/180)*
+			math.Sin(dLng/2)*math.Sin(dLng/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	// 单位：米
+	return R * c
+}
+
+func (b *BusStatus) Move(r *rand.Rand) error {
+	oldLatitude := b.Latitude
+	oldLongitude := b.Longitude
+	oldTime := time.Unix(0, b.Timestamp)
+	now := time.Now()
+	if oldTime.After(now) {
+		return fmt.Errorf("%s: Illegal timestamp argument", b.BusID)
+	}
+	// 纬度或经度变化约对应10米左右
+	maxDelta := 0.0001
+	deltaLatitude := (r.Float64()*2 - 1) * maxDelta
+	deltaLongitude := (r.Float64()*2 - 1) * maxDelta
+	newLatitude := oldLatitude + deltaLatitude
+	newLongitude := oldLongitude + deltaLongitude
+	// 根据haversine公式计算出来的距离
+	distance := haversine(oldLatitude, oldLongitude, newLatitude, newLongitude)
+	deltaTimestamp := now.Sub(oldTime).Seconds()
+	// m/s
+	speed := distance / deltaTimestamp
+	// 更新数据
+	b.Latitude = newLatitude
+	b.Longitude = newLongitude
+	b.Speed = speed
+	b.Timestamp = now.UnixNano()
+	return nil
 }
